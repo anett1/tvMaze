865db77aafb357c74c84b70c87cc2d46b96cc31e
Aneta Jagla <anett@poczta.fm>
2020-07-09 15:00:19 +0200

    Added navbar, animation for divCard, changed http protocol to https protocol

--- src/request.js
+++ src/request.js
@@ -1,11 +1,11 @@
 export const getShowsByKey = (key) => {
-  return fetch(`http://api.tvmaze.com/search/shows?q=${key}`).then((resp) =>
+  return fetch(`https://api.tvmaze.com/search/shows?q=${key}`).then((resp) =>
     resp.json()
   );
 };
 
 export const getShowById = (id) => {
-  return fetch(`http://api.tvmaze.com/shows/${id}?embed=cast`).then((resp) =>
+  return fetch(`https://api.tvmaze.com/shows/${id}?embed=cast`).then((resp) =>
     resp.json()
   );
 };
